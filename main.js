c20d91c92d50e12df52f57425201b90b7eb7bca7
Benereta Hox <beneretahoxha8@gmail.com>
2025-03-20 15:10:07 +0100

    early db version connected

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ import addMdToPage from './libs/addMdToPage.js';
 import dbQuery from "./libs/dbQuery.js";
 import tableFromData from './libs/tableFromData.js'
 
+addMdToPage("# 50 students");
+
+let students = await dbQuery("SELECT * FROM results LIMIT 50");
+tableFromData({ data: students });
