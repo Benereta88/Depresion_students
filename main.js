811185fcad26bfce98783adea15f0e7a27d25031
Benereta Hox <beneretahoxha8@gmail.com>
2025-03-31 13:49:23 +0200

    ny ändring
    commit

--- main.js
+++
@@ -1,9 +0,0 @@
-import './libs/liveReload.js';
-import addMdToPage from './libs/addMdToPage.js';
-import dbQuery from "./libs/dbQuery.js";
-import tableFromData from './libs/tableFromData.js'
-
-addMdToPage("# 50 students");
-
-let students = await dbQuery("SELECT * FROM results LIMIT 50");
-tableFromData({ data: students });
